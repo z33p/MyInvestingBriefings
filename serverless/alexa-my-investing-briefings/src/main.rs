41ca2a2ca0587d7fbccd8a92e56381ec2a77a6ea
z33p <raphael00fellipe@gmail.com>
2022-11-20 13:15:02 -0300

    feat: basice http lambda

--- serverless/alexa-my-investing-briefings/src/main.rs
+++ serverless/alexa-my-investing-briefings/src/main.rs
@@ -1,31 +1,50 @@
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
+// This example requires the following input to succeed:
+// { "command": "do something" }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(_: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
+use lambda_runtime::{service_fn, Error, LambdaEvent};
+use serde::{Deserialize, Serialize};
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body("Hello World Raphael".into())
-        .map_err(Box::new)?;
-    Ok(resp)
+/// This is also a made-up example. Requests come into the runtime as unicode
+/// strings in json format, which can map to any structure that implements `serde::Deserialize`
+/// The runtime pays no attention to the contents of the request payload.
+#[derive(Deserialize)]
+struct Request {
+    command: String,
+}
+
+/// This is a made-up example of what a response structure may look like.
+/// There is no restriction on what it can be. The runtime requires responses
+/// to be serialized into json. The runtime pays no attention
+/// to the contents of the response payload.
+#[derive(Serialize)]
+struct Response {
+    req_id: String,
+    msg: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
-        .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
 
-    run(service_fn(function_handler)).await
+    let func = service_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn my_handler(event: LambdaEvent<Request>) -> Result<Response, Error> {
+    // extract some useful info from the request
+    let command = event.payload.command;
+
+    // prepare the response
+    let resp = Response {
+        req_id: event.context.request_id,
+        msg: format!("Command {} executed.", command),
+    };
+
+    // return `Response` (it will be serialized to JSON automatically by the runtime)
+    Ok(resp)
 }
